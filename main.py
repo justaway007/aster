7f88fed86393fa081c032d0808ec7a97c085ca19
justaway007 <irfankarim00@gmail.com>
2025-12-15 21:59:42 +0800

    update game loop to lock fps to 60

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from logger import log_state
 
-# def game_loop(screen):
-#     while True:
-#         screen.fill("black")
-#         screen.display.flip()
 
 
 def main():
@@ -18,6 +14,8 @@ def main():
     game_loop(screen)
 
 def game_loop(screen):
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -25,6 +23,7 @@ def game_loop(screen):
                 return
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
